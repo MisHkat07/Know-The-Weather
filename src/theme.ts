7fa8d0cf4f98afd1f6f8b9978418a00279c6eece
MisHkat <mishkat606@gmail.com>
2023-02-03 00:06:55 +0600

    Theme interface

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,5 @@
-import darkBg from './assets/subtle-prism2.svg';
-import lightBg from './assets/subtle-prism.svg';
+import darkBg from "./assets/subtle-prism2.svg";
+import lightBg from "./assets/subtle-prism.svg";
 
 export interface Theme {
   appTitleColor: string;
@@ -30,57 +30,57 @@ export interface Theme {
 }
 
 export const lightTheme: Theme = {
-  appTitleColor: '#2F5D8A',
+  appTitleColor: "#2F5D8A",
   backgroundImage: lightBg,
   backgroundGradient: {
-    color1: '#F9FFFF',
-    color2: '#38C8E6',
+    color1: "#F9FFFF",
+    color2: "#38C8E6",
   },
-  panelBgColor: '#FFFFFF',
-  panelTitleColor: '#727E8E',
-  forecastPanelBgColor: 'rgba(255, 255, 255, 0.75)',
+  panelBgColor: "#FFFFFF",
+  panelTitleColor: "#727E8E",
+  forecastPanelBgColor: "rgba(255, 255, 255, 0.75)",
   searchInput: {
-    color: '#727E8E',
-    placeholderColor: '#6898d5',
+    color: "#727E8E",
+    placeholderColor: "#6898d5",
   },
   temperatureSwitch: {
-    backgroundColor: '#77b1c7',
-    sliderColor: '#fff',
-    textColor: '#fff',
+    backgroundColor: "#77b1c7",
+    sliderColor: "#fff",
+    textColor: "#fff",
   },
   searchSuggestion: {
-    backgroundColor: '#fff',
-    hoverBackgroundColor: '#f9f9f9',
-    seperatorLineColor: '#ccc',
+    backgroundColor: "#fff",
+    hoverBackgroundColor: "#f9f9f9",
+    seperatorLineColor: "#ccc",
   },
-  smallIconColor: '#A1B9CE',
-  smallIconTextColor: '#7B98B2',
+  smallIconColor: "#A1B9CE",
+  smallIconTextColor: "#7B98B2",
 };
 
 export const darkTheme: Theme = {
-  appTitleColor: '#2F5D8A',
+  appTitleColor: "#2F5D8A",
   backgroundImage: darkBg,
   backgroundGradient: {
-    color1: '#031027',
-    color2: '#02101D',
+    color1: "#031027",
+    color2: "#02101D",
   },
-  panelBgColor: '#051A33',
-  panelTitleColor: '#216397',
-  forecastPanelBgColor: 'rgba(5, 26, 51, 0.75)',
+  panelBgColor: "#051A33",
+  panelTitleColor: "#216397",
+  forecastPanelBgColor: "rgba(5, 26, 51, 0.75)",
   searchInput: {
-    color: '#5f8bbf',
-    placeholderColor: '#235A84',
+    color: "#5f8bbf",
+    placeholderColor: "#235A84",
   },
   temperatureSwitch: {
-    backgroundColor: '#1b3657',
-    sliderColor: '#437abd',
-    textColor: '#718cac',
+    backgroundColor: "#1b3657",
+    sliderColor: "#437abd",
+    textColor: "#718cac",
   },
   searchSuggestion: {
-    backgroundColor: '#0f2744',
-    hoverBackgroundColor: '#183553',
-    seperatorLineColor: '#356097',
+    backgroundColor: "#0f2744",
+    hoverBackgroundColor: "#183553",
+    seperatorLineColor: "#356097",
   },
-  smallIconColor: '#153C5E',
-  smallIconTextColor: '#3975AB',
+  smallIconColor: "#153C5E",
+  smallIconTextColor: "#3975AB",
 };
